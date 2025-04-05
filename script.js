d5e0a11178c761c008d443883cb32743911da0ec
mayankmahor21 <mahormayank1@gmail.com>
2025-04-05 17:11:44 +0530

    some chamge

--- script.js
+++ script.js
@@ -41,37 +41,57 @@ const lenis = new Lenis({
 
 
 
-const slide = document.querySelectorAll(".slide");
-var counter = 0;
+// const slide = document.querySelectorAll(".slide");
+// var counter = 0;
 
-// Set the position of each slide
-slide.forEach((slide, index) => {
-    slide.style.left = `${index * 100}%`;
-});
 
-// Function to go to the previous slide
-const goPrev = () => {
-    counter--;
-    if (counter < 0) {
-        counter = slide.length - 1; // Loop back to the last slide
-    }
-    slideImage();
-};
+// slide.forEach((slide, index) => {
+//     slide.style.left = `${index * 100}%`;
+// });
 
-// Function to go to the next slide
-const goNext = () => {
-    counter++;
-    if (counter >= slide.length) {
-        counter = 0; // Loop back to the first slide
-    }
-    slideImage();
-};
 
-// Function to update the slide position
-const slideImage = () => {
-    slide.forEach((slide) => {
-        slide.style.transform = `translateX(-${counter * 100}%)`;
-    });
-};
+// const goPrev = () => {
+//     counter--;
+//     if (counter < 0) {
+//         counter = slide.length - 1; 
+//     }
+//     slideImage();
+// };
 
+
+// const goNext = () => {
+//     counter++;
+//     if (counter >= slide.length) {
+//         counter = 0; 
+//     }
+//     slideImage();
+// };
+
+
+// const slideImage = () => {
+//     slide.forEach((slide) => {
+//         slide.style.transform = `translateX(-${counter * 100}%)`;
+//     });
+// };
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
